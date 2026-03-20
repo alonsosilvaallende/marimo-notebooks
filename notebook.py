19ed36290c2e80f9f23d3a45db7038f85163bb2d
Alonso Silva <alonso.silva@gmail.com>
2026-03-20 17:22:38 +0100

    Update notebook

--- notebook.py
+++ notebook.py
@@ -5,9 +5,28 @@ app = marimo.App(width="medium")
 
 
 @app.cell
+async def _():
+    # Function to detect automatically if this is a webassembly notebook
+    def is_webassembly_notebook():
+        import sys
+        return sys.platform == 'emscripten'
+
+    # If it is a webassembly notebook install the package on the fly
+    if is_webassembly_notebook():
+        import micropip
+        await micropip.install("graphviznb")
+    return
+
+
+@app.cell
 def _():
     import graphviznb
 
+    return (graphviznb,)
+
+
+@app.cell
+def _(graphviznb):
     w = graphviznb.Widget()
     w.source = """
     digraph {
